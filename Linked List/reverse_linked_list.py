4964aaa0d71d47790157c61cd04e89d60d3c81e4
Koushik Bhushan <koushikbhushan@gmail.com>
2018-03-20 23:39:17 +0530

    Reverse linked list

---
+++ Linked List/reverse_linked_list.py
@@ -0,0 +1,44 @@
+#Reverse a linked list
+class Node:
+	def __init__(self, data):
+		self.data = data
+		self.next_node = None
+
+class LinkedList:
+	def __init__(self):
+		self.head = None
+
+	def push(self, data):
+		node = Node(data)
+		node.next_node = self.head
+		self.head = node
+
+	def reverse(self):
+		prev = None
+		curr = self.head
+
+		while curr != None:
+			next = curr.next_node
+			curr.next_node = prev
+			prev = curr
+			curr = next
+		self.head = prev
+			
+	def print(self):
+		start = self.head
+		while start != None:
+			print(start.data, end=" ")
+			start = start.next_node
+		print()
+
+if __name__ == '__main__':
+	elements = list(map(int, input('Enter elements for linked list:').split()))
+	linked_list = LinkedList()
+	for i in elements:
+		linked_list.push(i)
+
+	print('Original list:' ,end=" ")
+	linked_list.print()
+	linked_list.reverse()	
+	print('Reverse list:' ,end=" ")
+	linked_list.print()
